6a194a508a6c2cf1c5c278f8e85a25b7ff73c421
gbmarkovsky@gmail.com <gbmarkovsky@gmail.com@c34d71c1-a45f-99b5-1689-fefd6dad9a12>
2011-03-16 20:24:46 +0000

    добавлен проект

---
+++ LogicEditor/src/com/gmail/gbmarkovsky/views/LogicCellView.java
@@ -0,0 +1,29 @@
+package com.gmail.gbmarkovsky.views;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+
+import com.gmail.gbmarkovsky.engine.Gate;
+
+/**
+ * Класс для отображения логического элемента.
+ * @author george
+ *
+ */
+public class LogicCellView extends AbstractGateView {
+
+	public LogicCellView(Point position, Gate gate) {
+		super(position, gate);
+	}
+
+	@Override
+	public void paint(Graphics g) {
+		g.setColor(Color.white);
+		g.fillRect(position.x, position.y, width, height);
+		g.setColor(Color.black);
+		g.drawRect(position.x, position.y, width, height);
+		g.drawString(gate.getType().getSymbol(), position.x + 5,  position.y + 12);
+	}
+
+}
